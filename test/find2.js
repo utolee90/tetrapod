0eaf71d2dbc35638e7d4b2767c5f008d330ab8f3
utolee90 <itdatabasewiki@gmail.com>
2022-07-25 18:45:19 +0900

    fix some err

--- test/find2.js
+++ test/find2.js
@@ -6,117 +6,121 @@ import Hangul from "hangul-js"
 import fs from 'fs';
 import * as HO from "../components/HangulObjects";
 
-// 테스트 시도
-// 우선 기본적으로 Utils의 함수들부터 실험해봅시다.
-let testList1 = ['가', ['내리', '다', '수'], [1, 4]]
-console.log('거짓임:::', Utils.objectEqual(testList1, ['가', ['내리', '다', '수'], ['1', '4']])) // 거짓
-console.log('참임:::', Utils.objectEqual(testList1, ['가', ['내리', '다', '수'], [1, 4]])) // 참
-// 두 번째 실험 - objectIn -
-console.log('참임:::', Utils.objectIn(['내리', '다', '수'], testList1)) // 참
-// 세 번째 실험 - productList
-let testList2 = Utils.productList(['가불', '가방', '사이좋게', '적용'], ['기술', '기능'], ['', ' 사용하기', ' 중단하기'])
-console.log('testList2 원소 확인하기')
-for (let elem of testList2) {
-    console.log(elem);
-}
-// 이제 본격적으로 실험해보자
-console.log()
-console.log('tetrapod 유틸리티 실험')
-console.log(Utils.wordToArray('가!ㄱ,ㅓ라+ 아니'))
-console.log()
-console.log('lengthSplit 실험')
-let longSentence = '옛날에 서로 우애가 좋은 형제가 살았습니다. 어느날 형제는 금덩어리를 발견했습니다.'
-let split = Utils.lengthSplit(longSentence, 12);
-console.log(split.length);
-for (let idx in split) {
-    console.log(idx*6, ':', split[idx])
-}
-console.log()
-console.log(Utils.wordToArray('가!ㄴㅏ+닥로ㄱ,ㅏ'))
-// console.log('recursiveComponent 실험') // 오류가 있다.
-// let recur = [['1', '2', '3', [['a', 'b', 'c'], ['x','y','z']], ['가', '나', [['다', '마'], ['라','바']]]]];
-// console.log(Utils.recursiveComponent(recur))
-
-console.log('disassemble 실험')
-console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'key', true))
-console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'part', true))
-console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'sound', true))
-console.log(Utils.disassemble('없쟈 완젼 짜증나짂'))
-console.log()
-// 각종 맵이 잘 작동하는지 확인해보자
-console.log('qwertyToDubeol 테스트')
-let testQwerty = Utils.qwertyToDubeol('dnflskfk가 가장 wkdkfkdtmfjqwl dksgdmsep? dho? 난 멀라.', true)
-console.log('PARSING')
-let parsed = Utils.parseMap(testQwerty)
-console.log(parsed)
-console.log('RESERVING')
-console.log(Utils.reserveMap(parsed))
-
-// antispoof 테스트
-console.log('antispoof 테스트')
-let testAntispoof = Utils.antispoof('ㄱH같이 ㅂy2고 저0스0같ㅇl 쓴다', true)
-let parsed2 = Utils.parseMap(testAntispoof)
-console.log(parsed2)
-console.log('RESERVING')
-console.log(Utils.reserveMap(parsed2))
-
-// dropDouble 테스트
-console.log('DROPDOUBLE 테스트')
-console.log('joinedSyllable', Utils.joinedSyllable('바','알'), Utils.joinedSyllable('삶', '하'))
-console.log(Utils.dropDouble("ql이바알놈아!!", true, false))
-console.log('')
-console.log(Utils.dropDouble("시이바알놈아!!", true, false));
-console.log('originalPosition 테스트')
-console.log(Utils.originalPosition(Utils.qwertyToDubeol('dnflskfk akstp wjdakf rnlcksgek', true), [2,3,4,6,7,8]));
-// dropDouble + simplify
-console.log('DROPDOUBLE + Simplify 테스트')
-let ddMap = Utils.dropDouble('아아아주 아아아주 복잡호아안 메에시이이이쥩', true, false)
-let ddsMap = Utils.dropDouble('아아아주 아아아주 복잡호아안 메에시이이이쥩', true, true)
-console.log(ddMap)
-console.log(Utils.parseMap(ddMap, true)) // dropDouble은 reassemble 조건 반드시 켜서 사용.
-console.log('simplify')
-console.log(ddsMap)
-console.log(Utils.parseMap(ddsMap, true)) // dropDouble은 reassemble 조건 반드시 켜서 사용.
-console.log('')
-//tooMuchDoubleEnd 테스트
-console.log('tooMuchDoubleEnd 테스트');
-console.log(Utils.tooMuchDoubleEnd('젊은 사람들아 값없이 지네'))
-console.log(Utils.tooMuchDoubleEnd('젊읆 사랆듦앎 값없이 짒넶'))
-console.log(Utils.tooMuchDoubleEnd('너 앍줎 없다.'))
-
-// engToKo 테스트
-let testPronounce = Utils.engToKo('dolai, dol-ai네 jarangoro jarang하지말자', true)
-console.log(Utils.parseMap(testPronounce))
-console.log('RESERVING')
-console.log(Utils.reserveMap(Utils.parseMap(testPronounce)))
-
-// dropDouble test
-// console.log('의 처리가 필요한가?')
-// console.log(Utils.dropDouble('듸어서', false, false))
-// console.log(Utils.dropDouble('듸어서', false, true))
-// console.log(Utils.dropDouble('저여', false, false))
-// console.log(Utils.dropDouble('져여', false, false))
-// console.log(Utils.dropDouble('저여', false, true))
-// console.log(Utils.dropDouble('져여', false, true))
+// // 테스트 시도
+// // 우선 기본적으로 Utils의 함수들부터 실험해봅시다.
+// let testList1 = ['가', ['내리', '다', '수'], [1, 4]]
+// console.log('거짓임:::', Utils.objectEqual(testList1, ['가', ['내리', '다', '수'], ['1', '4']])) // 거짓
+// console.log('참임:::', Utils.objectEqual(testList1, ['가', ['내리', '다', '수'], [1, 4]])) // 참
+// // 두 번째 실험 - objectIn -
+// console.log('참임:::', Utils.objectIn(['내리', '다', '수'], testList1)) // 참
+// // 세 번째 실험 - productList
+// let testList2 = Utils.productList(['가불', '가방', '사이좋게', '적용'], ['기술', '기능'], ['', ' 사용하기', ' 중단하기'])
+// console.log('testList2 원소 확인하기')
+// for (let elem of testList2) {
+//     console.log(elem);
+// }
+// // 이제 본격적으로 실험해보자
+// console.log()
+// console.log('tetrapod 유틸리티 실험')
+// console.log(Utils.wordToArray('가!ㄱ,ㅓ라+ 아니'))
+// console.log()
+// console.log('lengthSplit 실험')
+// let longSentence = '옛날에 서로 우애가 좋은 형제가 살았습니다. 어느날 형제는 금덩어리를 발견했습니다.'
+// let split = Utils.lengthSplit(longSentence, 12);
+// console.log(split.length);
+// for (let idx in split) {
+//     console.log(idx*6, ':', split[idx])
+// }
+// console.log()
+// console.log(Utils.wordToArray('가!ㄴㅏ+닥로ㄱ,ㅏ'))
+// // console.log('recursiveComponent 실험') // 오류가 있다.
+// // let recur = [['1', '2', '3', [['a', 'b', 'c'], ['x','y','z']], ['가', '나', [['다', '마'], ['라','바']]]]];
+// // console.log(Utils.recursiveComponent(recur))
+//
+// console.log('disassemble 실험')
+// console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'key', true))
+// console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'part', true))
+// console.log(Utils.disassemble('없쟈 완젼 짜증나짂', 'sound', true))
+// console.log(Utils.disassemble('없쟈 완젼 짜증나짂'))
+// console.log()
+// // 각종 맵이 잘 작동하는지 확인해보자
+// console.log('qwertyToDubeol 테스트')
+// let testQwerty = Utils.qwertyToDubeol('dnflskfk가 가장 wkdkfkdtmfjqwl dksgdmsep? dho? 난 멀라.', true)
+// console.log('PARSING')
+// let parsed = Utils.parseMap(testQwerty)
+// console.log(parsed)
+// console.log('RESERVING')
+// console.log(Utils.reserveMap(parsed))
+//
+// // antispoof 테스트
+// console.log('antispoof 테스트')
+// let testAntispoof = Utils.antispoof('ㄱH같이 ㅂy2고 저0스0같ㅇl 쓴다', true)
+// let parsed2 = Utils.parseMap(testAntispoof)
+// console.log(parsed2)
+// console.log('RESERVING')
+// console.log(Utils.reserveMap(parsed2))
+//
+// // dropDouble 테스트
+// console.log('DROPDOUBLE 테스트')
+// console.log('joinedSyllable', Utils.joinedSyllable('바','알'), Utils.joinedSyllable('삶', '하'))
+// console.log(Utils.dropDouble("ql이바알놈아!!", true, false))
+// console.log('')
+// console.log(Utils.dropDouble("시이바알놈아!!", true, false));
+// console.log('originalPosition 테스트')
+// console.log(Utils.originalPosition(Utils.qwertyToDubeol('dnflskfk akstp wjdakf rnlcksgek', true), [2,3,4,6,7,8]));
+// // dropDouble + simplify
+// console.log('DROPDOUBLE + Simplify 테스트')
+// let ddMap = Utils.dropDouble('아아아주 아아아주 복잡호아안 메에시이이이쥩', true, false)
+// let ddsMap = Utils.dropDouble('아아아주 아아아주 복잡호아안 메에시이이이쥩', true, true)
+// console.log(ddMap)
+// console.log(Utils.parseMap(ddMap, true)) // dropDouble은 reassemble 조건 반드시 켜서 사용.
+// console.log('simplify')
+// console.log(ddsMap)
+// console.log(Utils.parseMap(ddsMap, true)) // dropDouble은 reassemble 조건 반드시 켜서 사용.
+// console.log('')
+// //tooMuchDoubleEnd 테스트
+// console.log('tooMuchDoubleEnd 테스트');
+// console.log(Utils.tooMuchDoubleEnd('젊은 사람들아 값없이 지네'))
+// console.log(Utils.tooMuchDoubleEnd('젊읆 사랆듦앎 값없이 짒넶'))
+// console.log(Utils.tooMuchDoubleEnd('너 앍줎 없다.'))
+//
+// // engToKo 테스트
+// let testPronounce = Utils.engToKo('dolai, dol-ai네 jarangoro jarang하지말자', true)
+// console.log(Utils.parseMap(testPronounce))
+// console.log('RESERVING')
+// console.log(Utils.reserveMap(Utils.parseMap(testPronounce)))
+//
+// // dropDouble test
+// // console.log('의 처리가 필요한가?')
+// // console.log(Utils.dropDouble('듸어서', false, false))
+// // console.log(Utils.dropDouble('듸어서', false, true))
+// // console.log(Utils.dropDouble('저여', false, false))
+// // console.log(Utils.dropDouble('져여', false, false))
+// // console.log(Utils.dropDouble('저여', false, true))
+// // console.log(Utils.dropDouble('져여', false, true))
 
 let obj = new Tetrapod();
-obj.loadFile()
+// obj.loadFile();
+obj.loadFile('./resource/simplify/bad-words.json',
+    './resource/simplify/normal-words.json',
+    './resource/simplify/macros.json',
+    true)
 
 
-console.log('정상단어 위치', obj.findNormalWordPositions('흰색 옷은 아름답다', false))
-console.log('정상단어 위치', obj.nativeFind('컴퓨터 교육은 프로그래머의 씨발점이다', true, false))
-console.log('---------------------------------')
-console.log('오작동 테스트', Utils.isKindChar('점', '젖'))
-console.log('오작동 테스트', Utils.isKindChar('점', '젖', '이'))
-let simJut_I = []
-for (let i =44032; i<55204; i++) {
-    let char = String.fromCharCode(i);
-    if (Utils.isKindChar(char, '젖', '이')) simJut_I.push(char)
-}
-console.log('결과')
-for (let i=0; i<Math.ceil(simJut_I.length/10); i++) {
-    console.log(simJut_I.slice(i*10, (i+1)*10).join(', '))
-}
+// console.log('정상단어 위치', obj.findNormalWordPositions('흰색 옷은 아름답다', false))
+// console.log('정상단어 위치', obj.nativeFind('컴퓨터 교육은 프로그래머의 씨발점이다', true, false))
+// console.log('---------------------------------')
+// console.log('오작동 테스트', Utils.isKindChar('점', '젖'))
+// console.log('오작동 테스트', Utils.isKindChar('점', '젖', '이'))
+// let simJut_I = []
+// for (let i =44032; i<55204; i++) {
+//     let char = String.fromCharCode(i);
+//     if (Utils.isKindChar(char, '젖', '이')) simJut_I.push(char)
+// }
+// console.log('결과')
+// for (let i=0; i<Math.ceil(simJut_I.length/10); i++) {
+//     console.log(simJut_I.slice(i*10, (i+1)*10).join(', '))
+// }
 // let newTime = new Date().getTime()
 // console.log('시발 정발 나쁘게 구네 ::: TEST:::\n', JSON.stringify(obj.nativeFind("시발 정말 나쁘게 구네")))
 // console.log("걸린시간:::", new Date().getTime() - newTime)
@@ -128,7 +132,7 @@ for (let i=0; i<Math.ceil(simJut_I.length/10); i++) {
 // console.log(obj.nativeFind(Utils.msgToMap("너 앍줎 없다."), true, true, true))
 // console.log(obj.find("좆밥이네 아줎 싫어 새꺄", true))
 // console.log("걸린시간:::", new Date().getTime() - newTime)
-
+//
 // // 필터 켜기 실험
 // console.log('필터 켜기 전에 단어 확인', obj.parsedBadWords.length, Object.keys(obj.badWordsMap).length, obj.badWords[0].length)
 // obj.adjustFilter([],[],['qwerty'],false);
@@ -208,12 +212,14 @@ for (let i=0; i<Math.ceil(simJut_I.length/10); i++) {
 
 // fs.writeFile('./test.txt', JSON.stringify(Object.keys(obj.badWordsMap)), 'utf-8', (err)=> {console.log(err);})
 // console.log(obj.engBadWordsCheck([['지!','랄!']], 'ziral', 'pronounce', false))
-// obj.adjustFilter([],[],['qwerty', 'antispoof'], true);
-// newTime = new Date().getTime()
-// console.log(obj.find('Tlqkf 샏까 정말 ㅉH0하다!', false, 20))
-// console.log(new Date().getTime()-newTime); // 걸리는 시간 체크해보자.
-// console.log(obj.fix('Tlqkf 샏까 정말 ㅉH0하다!'))
-// console.log(new Date().getTime()-newTime); // 걸리는 시간 체크해보자.
+obj.adjustFilter([],[],[], true);
+console.log('test1', obj.find('Tlqkf 샏까 정말 ㅉH0하다!', false, 20))
+obj.adjustFilter([],[],['qwerty', 'antispoof'], true);
+let newTime = new Date().getTime()
+console.log('test2', obj.find('Tlqkf 샏까 정말 ㅉH0하다!', false, 20))
+console.log(new Date().getTime()-newTime); // 걸리는 시간 체크해보자.
+console.log(obj.fix('Tlqkf 샏까 정말 ㅉH하다!'))
+console.log(new Date().getTime()-newTime); // 걸리는 시간 체크해보자.
 
 // console.log('TEST')
 // console.log('return', Utils.getPositionAll('과랑뷔에 옷 젖는 줄 가랑비', '가!랑!비!', true, true))
---
+++ test/find3.js
@@ -0,0 +1,33 @@
+import Utils from "../components/Utils";
+import Tetrapod from "../tetrapod";
+import fs from 'fs';
+
+// 고추로 실험해보자. 얼마나 많이 모이는지 확인
+// 다른 단어 병신
+let isKindByeongsin = []
+let testTime = new Date().getTime();
+for (let i=44032; i<55204; i++) {
+    let firstChar = String.fromCharCode(i);
+    let tested = false;
+    if (Utils.isKindChar(firstChar, '병', '신')) {
+        for (let j=44032; j<55204; j++) {
+            let secondChar = String.fromCharCode(j);
+            if (Utils.isKindChar(secondChar, '신', '')) {
+                isKindByeongsin.push(firstChar+secondChar);
+                tested = true;
+            }
+        }
+    }
+    if (Utils.isKindChar(firstChar, '븅', '신')) {
+        for (let j=44032; j<55204; j++) {
+            let secondChar = String.fromCharCode(j);
+            if (Utils.isKindChar(secondChar, '신', '') && isKindByeongsin.indexOf(firstChar+secondChar) === -1) {
+                isKindByeongsin.push(firstChar+secondChar);
+                tested=true;
+            }
+        }
+    }
+    if(tested) {console.log('one', new Date().getTime() - testTime);}
+}
+let resString = isKindByeongsin.join("\n")
+fs.writeFile('./byeongsin.txt', resString, 'utf-8', (err)=> {console.log(err);})
